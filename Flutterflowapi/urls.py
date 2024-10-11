097e254b10703f8588b62791f9704ee0a499c575
Ahsan Ali <ahsanalishah48@gmail.com>
2024-10-11 18:20:52 +0500

    Update urls.py

--- Flutterflowapi/urls.py
+++ Flutterflowapi/urls.py
@@ -18,9 +18,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from Fileapi.views import ProjectImageUploadView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('upload-images/', ProjectImageUploadView.as_view(), name='upload-images'),
     path('api/', include('Fileapi.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
 
