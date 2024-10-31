f5933c7a67d80a9c54930688be45baed97c30ead
Ahsan Ali <ahsanalishah48@gmail.com>
2024-10-31 11:17:48 +0500

    Update urls.py

--- Fileapi/urls.py
+++ Fileapi/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from .views import FilePathUploadView
-from .views import ProjectImageUploadView
+
 
 urlpatterns = [
-    path('upload/', FilePathUploadView.as_view(), name='file-path-upload'),
-    path('api/', ProjectImageUploadView.as_view(), name='project-id'),
+    path('upload/', FilePathUploadView.as_view(), name='file-path-upload')
+   
   
 ]
 
